184881ff056e6866150c889d7ce10bf7dae2039d
maikerumine <michaellorenger@yahoo.com>
2015-12-09 18:34:24 -0500

    Readme

---
+++ README.lua
@@ -0,0 +1,29 @@
+--Technic Node Remover created by maikerumine
+-- Minetest 0.4.13 mod: "Technic Node Remover"
+-- namespace: tecnodrem
+--https://github.com/maikerumine
+
+--License:
+--~~~~~~~~
+--Code:
+--(c) Copyright 2015 maikerumine; modified zlib-License
+--see "LICENSE.txt" for details.
+
+--Media(if not stated differently):
+--(c) Copyright (2014-2015) maikerumine; CC-BY-SA 3.0
+
+
+--WARNING!!! IF YOU USE THIS THIS WILL PERMANTLY REMOVE ALL TECHNIC NODES FROM YOUR MAP!!!
+--The items in the machines will be retrievable.
+--The nodes will be replaced with high value blocks.
+
+
+
+
+
+
+
+
+
+
+
